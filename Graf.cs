a9e8e6d2ad9544411578c6d0da4db045413bc494
eduard <eduard0797>
2019-05-27 23:40:39 +0400

    first commit

---
+++ Graf.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CircleList
+{
+    
+    class Graf
+    {
+        // количество вершин
+        const int n = 4;
+        // количество связей
+        const int m = 5;
+        // вспомогательный список обработанных вершин
+        static bool[] visited;
+        // смежности
+        static int[,] graphSM = new int[n,n] {
+        /*1*/         { 0, 1, 0, 1 },
+        /*2*/         { 1, 0, 1, 1 },
+        /*3*/         { 0, 1, 0, 1 },
+        /*4*/         { 1, 1, 1, 0 }
+        };
+
+        static int[,] graphI = new int[n, m] {
+                //      a  b  c  d  e          
+        /*1*/         { 1, 1, 0, 0, 0 },
+        /*2*/         { 1, 0, 1, 1, 0 },
+        /*3*/         { 0, 0, 0, 1, 1 },
+        /*4*/         { 0, 1, 1, 0, 1 }
+        };
+
+
+
+
+        static void Main(string[] args)
+        {
+            // задать стартовую вершину(аналог корневой вершины при обходе дерева).
+            int index = 3;
+
+            visited = new bool[n];
+            DfsSM(index - 1);
+            Console.WriteLine();
+
+            //visited = new bool[n];
+            //BfsSM(index - 1);
+            //Console.WriteLine();
+
+            visited = new bool[n];
+            DfsI(index - 1);
+            Console.WriteLine();
+
+            //visited = new bool[n];
+            //BfsI(index - 1);
+            //Console.WriteLine();
+
+            Console.Read();
+        }
+
+        ////////////////////////смежности
+
+        //Поиск в глубину (англ. Depth-first search, DFS) для смежностей  -- рекурсивно
+        static void DfsSM(int vertex)
+        {
+            //обработать стартовую вершину
+            Console.Write(vertex + 1 + " ");
+            //паметить как обработанную
+            visited[vertex] = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                //повторить для всех смежных с vertex, которые еще необработаны
+                if ((graphSM[vertex, i] != 0) && (!visited[i]))
+                {
+                    DfsSM(i);
+                }
+            }
+        }
+
+        //Поиск в ширину (англ. breadth-first search, BFS)  для смежностей
+        static void BfsSM(int vertex)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(vertex);
+
+            Console.Write(vertex + 1 + " ");
+            visited[vertex] = true;
+
+            while (queue.Count != 0)
+            {
+                int pointer = queue.Dequeue();
+                // проходимся по всем смежным вершинам
+                for (int i = 0; i < n; i++)
+                {
+                    if ((graphSM[pointer, i] != 0) && (!visited[i]))
+                    {
+                        Console.Write(i + 1 + " ");
+                        // если вершина не была обработана ранее, то добавляем ее
+                        queue.Enqueue(i);
+                        visited[i] = true;
+                    }
+                }
+            }
+        }
+
+        ////////////////////////инциндентности
+
+
+        //Поиск в глубину (англ. Depth-first search, DFS) для инциндентностей
+        static void DfsI(int vertex)
+        {
+            //обработать стартовую вершину
+            Console.Write(vertex + 1 + " ");
+            //паметить как обработанную
+            visited[vertex] = true;
+
+            for (int i = 0; i < n; i++)
+            {
+                // находим инциндетное ребро
+                if (graphI[vertex, i] != 0)
+                {
+                    int temp = 0;
+                    // находим инциндентную по ребру вершину
+                    while (graphI[temp, i] != 1 || vertex == temp)
+                    {
+                        temp++;
+                    }
+                    if (!visited[temp])
+                    {
+                        DfsI(temp);
+                    }
+                }
+            }
+        }
+
+        //Поиск в ширину (англ. breadth-first search, BFS)   для инциндентностей
+        static void BfsI(int vertex)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(vertex);
+
+            Console.Write(vertex + 1 + " ");
+            visited[vertex] = true;
+
+            while (queue.Count != 0)
+            {
+                int pointer = queue.Dequeue();
+                for (int i = 0; i < m; i++)
+                {
+                    // находим инциндетное ребро
+                    if (graphI[pointer, i] != 0)
+                    {
+                        int temp = 0;
+                        // находим инциндентную по ребру вершину
+                        while (graphI[temp, i] != 1 || pointer == temp)
+                        {
+                            temp++;
+                        }
+                        if (!visited[temp])
+                        {
+                            Console.Write(temp + 1 + " ");
+                            queue.Enqueue(temp);
+                            visited[temp] = true;
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleList
+{
+    class Program
+    {
+        class CircleList
+        {
+            private int value;
+            private CircleList prev;
+            private CircleList next;
+
+            public CircleList(int value)
+            {
+                Value = value;
+            }
+
+            public int Value { get => value; set => this.value = value; }
+            public CircleList Prev { get => prev; set => prev = value; }
+            public CircleList Next { get => next; set => next = value; }
+
+            public static CircleList CreateHead(int value)
+            {
+                CircleList head = new CircleList(value);
+                head.Next = head;
+                head.Prev = head;
+                return head;
+            }
+
+            public void Add(int value)
+            {
+                CircleList newElement = new CircleList(value);
+                AddElement(ref newElement);
+            }
+
+            public void AddElement(ref CircleList newElement)
+            {
+                if (Next == null)
+                {
+                    Next = newElement;
+                }
+                else
+                {
+                    newElement.Next = Next;
+                    newElement.Next.Prev = newElement;
+                    Next = newElement;
+                }
+                newElement.Prev = this;
+            }
+
+            public void Delete(ref CircleList head)
+            {
+                // if head is alone
+                if (this == this.Prev)
+                {
+                    head = null;
+                    return;
+                }
+
+                // if head is not alone
+                if (this == head)
+                {
+                    head = this.Prev;
+                }
+
+                // if isn
+                this.Prev.Next = this.Next;
+                this.Next.Prev = this.Prev;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CircleList head = CircleList.CreateHead(1);
+            PrintList(ref head);
+            head.Add(2);
+            PrintList(ref head);
+            head.Add(3);
+            PrintList(ref head);
+            CircleList element = new CircleList(20);
+            head.AddElement(ref element);
+            PrintList(ref head);
+            head.Add(4);
+            PrintList(ref head);
+            head.Add(5);
+            PrintList(ref head);
+            element.Add(21);
+            PrintList(ref head);
+
+
+            element.Delete(ref head);
+            PrintList(ref head);
+
+
+            CircleList temp = FindByValueFromHead(ref head, 2);
+            Console.Read();
+
+
+        }
+
+        static CircleList FindByValueFromHead(ref CircleList head, int value)
+        {
+            if (head.Value == value)
+            {
+                return head;
+            }
+
+            CircleList item = head.Next;
+            while (item != head)
+            {
+                if (item.Value == value)
+                {
+                    return item;
+                }
+                item = item.Next;
+            }
+            return null;
+        }
+
+        static void PrintList(ref CircleList head)
+        {
+            CircleList item = head;
+            do
+            {
+                Console.Write(item.Value + "\t");
+                item = item.Next;
+            } while (item != head);
+            Console.WriteLine();
+        }
+    }
+}
